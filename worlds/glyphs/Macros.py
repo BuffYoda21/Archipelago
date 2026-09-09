d13231d006d8657847d9cdd3594909e8bdd5e04c
BuffYoda21 <buffyoda21@gmail.com>
2026-09-09 15:32:05 -0500

    GLYPHS: code cleanup

--- worlds/glyphs/Macros.py
+++ worlds/glyphs/Macros.py
@@ -9,7 +9,7 @@ from .Types import ButtonColor
 if TYPE_CHECKING:
     from . import GlyphsWorld
 
-def set_macro_rules(state: CollectionState, world: "GlyphsWorld") -> None:
+def set_macro_rules(world: "GlyphsWorld") -> None:
     player = world.player
 
     if world.options.LogicalWallJumps.value:
@@ -66,7 +66,8 @@ def can_wall_jump(state: CollectionState, world: "GlyphsWorld") -> bool:
 # def can_chain_wall_jumps(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
 #     return can_wall_jump(state, player, world) and world.options.LogicalWallJumpChains.value
 
-def can_press_buttons(state: CollectionState, player: int, world: "GlyphsWorld", button_list: list[str], allowedFaults: int=0) -> bool:
+def can_press_buttons(state: CollectionState, world: "GlyphsWorld", button_list: list[str], allowedFaults: int=0) -> bool:
+    player = world.player
     items = state.prog_items[player]
     prog_dash = items["Progressive Dash Orb"]
     has_parry = items["Progressive Parry"] >= 1
@@ -92,53 +93,50 @@ def can_press_buttons(state: CollectionState, player: int, world: "GlyphsWorld",
 
     return True
 
-def can_press_button(state: CollectionState, player: int, world: "GlyphsWorld", key: str) -> bool:
-    return can_press_buttons(state, player, world, [key])
-
-def between_buttons_missing(state: CollectionState, player: int, world: "GlyphsWorld") -> int:
+def between_buttons_missing(state: CollectionState, world: "GlyphsWorld") -> int:
     """
     DEPRICATED
     Use between_completion instead
     """
     count = 0
-    if not can_press_button(state, player, world, "Between rm1"):
+    if not can_press_buttons(state, world, ["Between rm1"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm4"):
+    if not can_press_buttons(state, world, ["Between rm4"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm5"):
+    if not can_press_buttons(state, world, ["Between rm5"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm10"):
+    if not can_press_buttons(state, world, ["Between rm10"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm13"):
+    if not can_press_buttons(state, world, ["Between rm13"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm19"):
+    if not can_press_buttons(state, world, ["Between rm19"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm28"):
+    if not can_press_buttons(state, world, ["Between rm28"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm30"):
+    if not can_press_buttons(state, world, ["Between rm30"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm39"):
+    if not can_press_buttons(state, world, ["Between rm39"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm40"):
+    if not can_press_buttons(state, world, ["Between rm40"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm43 Button 1"):
+    if not can_press_buttons(state, world, ["Between rm43 Button 1"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm43 Button 2"):
+    if not can_press_buttons(state, world, ["Between rm43 Button 2"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm57"):
+    if not can_press_buttons(state, world, ["Between rm57"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm66"):
+    if not can_press_buttons(state, world, ["Between rm66"]):
         count += 1
-    if not can_press_button(state, player, world, "Between rm71"):
+    if not can_press_buttons(state, world, ["Between rm71"]):
         count += 1
-    if not can_press_button(state, player, world, "Between Pre-Boss 1"):
+    if not can_press_buttons(state, world, ["Between Pre-Boss 1"]):
         count += 1
     return count
 
-def between_completion(state: CollectionState, player: int, world: "GlyphsWorld", missing_allowed: int=0) -> bool:
+def between_completion(state: CollectionState, world: "GlyphsWorld", missing_allowed: int=0) -> bool:
     buttons_to_check = ["Between rm1", "Between rm4", "Between rm5", "Between rm10", "Between rm13", "Between rm19", "Between rm28", "Between rm30", "Between rm39",
                         "Between rm40", "Between rm43 Button 1", "Between rm43 Button 2", "Between rm57", "Between rm66", "Between rm71", "Between Pre-Boss 1"]
-    return can_press_buttons(state, player, world, buttons_to_check, missing_allowed)
+    return can_press_buttons(state, world, buttons_to_check, missing_allowed)
 
 def can_press_green_buttons(state: CollectionState, player: int) -> bool:
     return state.has("Progressive Sword", player, 1) or state.has("Progressive Dash Orb", player, 2)
@@ -155,14 +153,16 @@ def has_grapple(state: CollectionState, player: int) -> bool:
 def can_parry(state: CollectionState, player: int) -> bool:
     return state.has("Progressive Parry", player, 1)
 
-def serpent_door_open(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
-    return state.has("Serpent Lock 1", player) and state.has("Serpent Lock 2", player) and state.has("Serpent Lock 3", player) and can_press_button(state, player, world, "R2B Gate Left")
+def serpent_door_open(state: CollectionState, world: "GlyphsWorld") -> bool:
+    player = world.player
+    return state.has("Serpent Lock 1", player) and state.has("Serpent Lock 2", player) and state.has("Serpent Lock 3", player) and can_press_buttons(state, world, ["R2B Gate Left"])
 
 def stalker_sigils_present(state: CollectionState, player: int) -> bool:
     return state.has("False Ending", player)
 
-def shadow_chase_open(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
-    return state.has("Stalker Sigil 1", player) and state.has("Stalker Sigil 2", player) and state.has("Stalker Sigil 3", player) and can_press_button(state, player, world, "R2J Gate Left")
+def shadow_chase_open(state: CollectionState, world: "GlyphsWorld") -> bool:
+    player = world.player
+    return state.has("Stalker Sigil 1", player) and state.has("Stalker Sigil 2", player) and state.has("Stalker Sigil 3", player) and can_press_buttons(state, world, ["R2J Gate Left"])
 
 def has_clarity(state: CollectionState, player: int) -> bool:
     return state.has("Clarity", player)
@@ -217,21 +217,22 @@ def can_solve_flower_puzzle(state: CollectionState, player: int) -> bool:
     """
     return can_start_flower_puzzle(state, player) and defeated_gilded_serpent(state, player) and can_dash(state, player) and can_press_green_buttons(state, player) and has_grapple(state, player)
 
-def flower_puzzle_completion(state: CollectionState, player: int, world: "GlyphsWorld") -> int:
+def flower_puzzle_completion(state: CollectionState, world: "GlyphsWorld") -> int:
     """
     If used in an entrance access rule, wrap with `multiworld.register_indirect_condition(world.get_region("Region 1E"), <foo>)`.
     """
+    player = world.player
     completion = 0
     wall_jump = can_wall_jump(state, world)
-    if state.can_reach_region("Region 1E", player) and can_dash(state, player) and can_press_buttons(state, player, world, ["R1B 4th Lowest", "R1B 5th Lowest"]) and (wall_jump or can_press_button(state, player, world, "R1B 6th Lowest")):
+    if state.can_reach_region("Region 1E", player) and can_dash(state, player) and can_press_buttons(state, world, ["R1B 4th Lowest", "R1B 5th Lowest"]) and (wall_jump or can_press_buttons(state, world, ["R1B 6th Lowest"])):
         completion += 1
     else:
         return completion
-    if can_press_buttons(state, player, world, ["R1B Map Room", "R2A Gate Left", "R2A Upper", "R2G Lower", "R2G Middle", "R2G Moving Platform", "R2N Chase 1"]) and (wall_jump or can_press_button(state, player, world, "R1F Right")) and (can_parry(state, player) or (get_button_color(world, "R2G Upper Middle") != ButtonColor.BLACK and can_press_buttons(state, player, world, ["R2G Upper Left", "R2G Upper Middle"]))):
+    if can_press_buttons(state, world, ["R1B Map Room", "R2A Gate Left", "R2A Upper", "R2G Lower", "R2G Middle", "R2G Moving Platform", "R2N Chase 1"]) and (wall_jump or can_press_buttons(state, world, ["R1F Right"])) and (can_parry(state, player) or (get_button_color(world, "R2G Upper Middle") != ButtonColor.BLACK and can_press_buttons(state, world, ["R2G Upper Left", "R2G Upper Middle"]))):
         completion += 1
     else:
         return completion
-    if defeated_gilded_serpent(state, player) and has_grapple(state, player) and can_press_button(state, player, world, "R2P Left"):
+    if defeated_gilded_serpent(state, player) and has_grapple(state, player) and can_press_buttons(state, world, ["R2P Left"]):
         completion += 1
     return completion
 
@@ -263,9 +264,9 @@ def can_access_all_silver_shards(state: CollectionState, world: "GlyphsWorld") -
     player = world.player
     return (
         can_dash(state, player) and has_grapple(state, player) and (defeated_gilded_serpent(state, player) or can_fight(state, world)) and
-        (can_wall_jump(state, world) or can_press_buttons(state, player, world, ["R1B Lowest", "R1B 3rd Lowest", "R1B 6th Lowest"])) and
-        (can_parry(state, player) or (get_button_color(world, "R2G Upper Middle") != ButtonColor.BLACK and can_press_buttons(state, player, world, ["R2G Upper Left", "R2G Upper Middle"]))) and
-        can_press_buttons(state, player, world, ["R1B 4th Lowest", "R1B 5th Lowest", "R2E Lower", "R2G Middle", "R2G Moving Platform", "R2N Chase 1", "R3E Upper"]) and
+        (can_wall_jump(state, world) or can_press_buttons(state, world, ["R1B Lowest", "R1B 3rd Lowest", "R1B 6th Lowest"])) and
+        (can_parry(state, player) or (get_button_color(world, "R2G Upper Middle") != ButtonColor.BLACK and can_press_buttons(state, world, ["R2G Upper Left", "R2G Upper Middle"]))) and
+        can_press_buttons(state, world, ["R1B 4th Lowest", "R1B 5th Lowest", "R2E Lower", "R2G Middle", "R2G Moving Platform", "R2N Chase 1", "R3E Upper"]) and
         state.can_reach_region("Region 1A", player) and
         state.can_reach_region("Region 1B", player) and
         state.can_reach_region("Region 1F", player) and
